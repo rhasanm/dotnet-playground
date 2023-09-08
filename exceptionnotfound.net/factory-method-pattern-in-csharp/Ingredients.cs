c1a7e531669a2fce14cbc89add4b05fa5e5d9a84
Md. Rakibul Hasan <hasanrakibul.masum@gmail.com>
2023-09-08 15:58:52 +0600

    factory design pattern

---
+++ exceptionnotfound.net/factory-method-pattern-in-csharp/Ingredients.cs
@@ -0,0 +1,88 @@
+namespace FactoryMethod
+{
+	// product
+	abstract class Ingredient {}
+
+	// concrete produc
+	class Bread : Ingredient {}
+	class Turkey : Ingredient {}
+    class Lettuce : Ingredient {}
+    class Mayonnaise : Ingredient {}
+
+	// creator
+	abstract class Sandwich
+	{
+		private List<Ingredient> _ingredient = new List<Ingredient>();
+
+		public Sandwich()
+		{
+			CreateIngredients();
+		}
+
+		// factory method
+		public abstract void CreateIngredients();
+
+		public List<Ingredient> Ingredients
+		{
+			get {return _ingredient;}
+		}
+	}
+
+	// concrete creator
+	class TurkeySandwich : Sandwich
+    {
+        public override void CreateIngredients()
+        {
+            Ingredients.Add(new Bread());
+            Ingredients.Add(new Mayonnaise());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Bread());
+        }
+    }
+
+	class Dagwood : Sandwich
+    {
+        public override void CreateIngredients()
+        {
+            Ingredients.Add(new Bread());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Mayonnaise());
+            Ingredients.Add(new Bread());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Mayonnaise());
+            Ingredients.Add(new Bread());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Mayonnaise());
+            Ingredients.Add(new Bread());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Mayonnaise());
+            Ingredients.Add(new Bread());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Mayonnaise());
+            Ingredients.Add(new Bread());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Mayonnaise());
+            Ingredients.Add(new Bread());
+        }
+    }
+}
---
+++ exceptionnotfound.net/factory-method-pattern-in-csharp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FactoryMethod
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Sandwich> sandwiches = new List<Sandwich>();
+            sandwiches.Add(new TurkeySandwich());
+            sandwiches.Add(new Dagwood());
+
+            foreach(var sandwich in sandwiches)
+            {
+                Console.WriteLine("\nSandwich: " + sandwich.GetType().Name + " ");
+                foreach(var ingredient in sandwich.Ingredients)
+                {
+                    Console.WriteLine("Ingredient: " + ingredient.GetType().Name);
+                }
+            }
+		}
+	}
+}
