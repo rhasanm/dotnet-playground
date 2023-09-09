cde6586ca7e705983f577e38c2c50fe3cb180c43
Md. Rakibul Hasan <hasanrakibul.masum@gmail.com>
2023-09-09 07:07:26 +0600

    singleton design pattern

---
+++ exceptionnotfound.net/singleton-pattern-in-csharp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bell = TheBell.Instance;
+            bell.Ring();
+
+            // Console.ReadKey();
+        }
+    }
+}
---
+++ exceptionnotfound.net/singleton-pattern-in-csharp/TheBell.cs
@@ -0,0 +1,31 @@
+namespace Singleton
+{
+	public sealed class TheBell
+	{
+        private static TheBell bellConnection;
+        private static object syncRoot = new Object();
+        private TheBell()
+        {
+
+        }
+        public static TheBell Instance
+        {
+            get
+            {
+                lock(syncRoot)
+                {
+                    if(bellConnection == null)
+                    {
+                        bellConnection = new TheBell();
+                    }
+                }
+
+                return bellConnection;
+            }
+        }
+        public void Ring()
+        {
+            Console.WriteLine("Ding! Order up!");
+        }
+	}
+}
